fbdcb9c5d9dde786b90cc76aa52b956b0e6c1b46
Travis CI <travis@travis-ci.org>
2020-06-06 10:36:20 +0000

    'Deploy project with Ts.ED v5.57.1'

--- test/integration/controllers/rest.spec.ts
+++ test/integration/controllers/rest.spec.ts
@@ -1,3 +1,4 @@
+import {PlatformTest} from "@tsed/common/src";
 import {TestContext} from "@tsed/testing";
 import {expect} from "chai";
 import * as SuperTest from "supertest";
@@ -5,26 +6,26 @@ import {Server} from "../../../src/Server";
 import {ExpressApplication} from "@tsed/common";
 
 describe("Session", () => {
-  let agent;
-
+  let request: SuperTest.SuperTest<SuperTest.Test>;
+  after(PlatformTest.reset);
   before(TestContext.bootstrap(Server));
-  before(TestContext.inject([ExpressApplication], async (expressApplication: ExpressApplication) => {
-    agent = SuperTest.agent(expressApplication);
-  }));
+  before(() => {
+    request = SuperTest.agent(PlatformTest.callback());
+  });
   after(TestContext.reset);
 
   describe("Login / Logout", () => {
     it("should create session return hello world and connect a fake user", async () => {
       // WHEN
-      const response1 = await agent.get("/rest/whoami").expect(200);
+      const response1 = await request.get("/rest/whoami").expect(200);
 
-      await agent.post("/rest/login").send({name: "UserName"}).expect(204);
+      await request.post("/rest/login").send({name: "UserName"}).expect(204);
 
-      const response2 = await agent.get("/rest/whoami").expect(200);
+      const response2 = await request.get("/rest/whoami").expect(200);
 
-      await agent.post("/rest/logout").expect(204);
+      await request.post("/rest/logout").expect(204);
 
-      const response3 = await agent.get("/rest/whoami").expect(200);
+      const response3 = await request.get("/rest/whoami").expect(200);
 
       // THEN
       expect(response1.text).to.eq("Hello world");
