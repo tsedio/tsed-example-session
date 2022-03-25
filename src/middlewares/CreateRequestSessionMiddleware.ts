642e60ad31481865f50b8bfc153ae92c1d10e343
doebling <88392704+doebling@users.noreply.github.com>
2022-03-26 00:35:06 +0100

    Update CreateRequestSessionMiddleware

--- src/middlewares/CreateRequestSessionMiddleware.ts
+++ src/middlewares/CreateRequestSessionMiddleware.ts
@@ -1,8 +1,8 @@
-import {Middleware, Req} from "@tsed/common";
+import {Middleware, PlatformRequest} from "@tsed/common";
 
 @Middleware()
 export class CreateRequestSessionMiddleware {
-  use(@Req() request: Req) {
+  use(@Req() request: PlatformRequest) {
     if (request.session) {
       request.session.user = request.session.user || {
         id: null
